2f6437c24030a096018b7933362e58c261d56e95
SMDAIYAN <shahdaiyan12@gmail.com>
2024-02-05 12:59:47 +0600

    completed grade calculator task

---
+++ gradeCalculator.js
@@ -0,0 +1,18 @@
+// grade calculator
+
+let grade = 55;
+
+if (grade >= 90) {
+    console.log('A')
+}
+
+else if (grade >=70) {
+    console.log('C')
+}
+else if (grade >=60) {
+    console.log('D')
+}
+else if (grade >=0) {
+    console.log('F')
+}
+
